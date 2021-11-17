2b73c7c5e43674c4d26a93df5b97327d9e663f70
maeneak <20371477+maeneak@users.noreply.github.com>
2021-11-17 15:02:46 +0000

    Remove redundant files

--- tests/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for Binance Balance tests."""
-from custom_components.binance_balance.const import (
-    CONF_API_SECRET,
-)
-from custom_components.binance_balance.const import (
-    CONF_API_KEY,
-)
-
-MOCK_CONFIG = {CONF_API_KEY: "test_api", CONF_API_SECRET: "test_secret"}
