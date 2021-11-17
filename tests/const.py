0682733168a8553e51e4623e5e59c49a0a86bbd0
maeneak <20371477+maeneak@users.noreply.github.com>
2021-11-17 19:07:35 +1000

    Initial

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Binance Balance tests."""
+from custom_components.binance_balance.const import (
+    CONF_PASSWORD,
+)
+from custom_components.binance_balance.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
